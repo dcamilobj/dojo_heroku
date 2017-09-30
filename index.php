0677cc65cf8e36d5122d8133f6de0adf722aa00a
dcamilobj <dcamilobj@gmail.com>
2017-09-30 08:39:24 -0500

    Added heroku dojo file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>My first PHP page</h1>
+    <?php echo "Hello App empresariales - Duban Camilo Bedoya!"; ?>
+  </body>
+</html>
